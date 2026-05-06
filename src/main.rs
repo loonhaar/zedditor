ace5c6ee0c2b861eb66f3505c2451e803cb13670
cipher0ne <199516992+loonhaar@users.noreply.github.com>
2026-05-06 22:29:22 +0200

    Implement a layout with the main area and a command area

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+use std::fmt::Debug;
+
 use color_eyre::eyre::{Ok, Result};
 use ratatui::{
 	DefaultTerminal, Frame,
@@ -5,24 +8,44 @@ use ratatui::{
 	layout::{Constraint, Layout},
 	style::{Color, Stylize},
 	text::ToSpan,
-	widgets::{Block, Widget},
+	widgets::Block,
 };
 
+#[derive(Debug, Default)]
+enum Mode {
+	#[default]
+	Command,
+	//Edit, // TODO: make the borders and titles yellow in edit mode
+}
+
+impl fmt::Display for Mode {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{:?}", self)
+	}
+}
+
+#[derive(Debug, Default)]
+struct AppState {
+	mode: Mode,
+	// TODO: buffer,
+}
+
 fn main() -> Result<()> {
+	let mut app_state = AppState::default();
+
 	color_eyre::install()?;
 
 	let terminal = ratatui::init();
-	let result = run(terminal);
+	let result = run(terminal, &mut app_state);
 
 	ratatui::restore();
 	result
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
+fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
 	loop {
-		terminal.draw(|t| render(t))?;
+		terminal.draw(|t| render(t, app_state))?;
 
-		// TODO mode switching via key events
 		if let Event::Key(key) = event::read()? {
 			match key.code {
 				event::KeyCode::Esc => {
@@ -35,22 +58,32 @@ fn run(mut terminal: DefaultTerminal) -> Result<()> {
 	Ok(())
 }
 
-fn render(frame: &mut Frame) {
-	let [border_area] = Layout::vertical([Constraint::Fill(1)])
-		.margin(0)
-		.areas(frame.area());
+fn render(frame: &mut Frame, app_state: &mut AppState) {
+	let layout = Layout::default()
+		.direction(ratatui::layout::Direction::Vertical)
+		.constraints(vec![
+			Constraint::from_mins([3, 3]),
+			Constraint::from_maxes([65535, 3]),
+		])
+		.split(frame.area());
 
 	// Text area
-	Block::bordered()
-		.border_type(ratatui::widgets::BorderType::Rounded)
-		.fg(Color::LightGreen)
-		.title(" Zedditor ".to_span().into_centered_line())
-		.render(border_area, frame.buffer_mut());
+	frame.render_widget(
+		Block::bordered()
+			.border_type(ratatui::widgets::BorderType::Rounded)
+			.fg(Color::LightGreen)
+			.title(" Zedditor ".to_span().into_centered_line()),
+		layout[0],
+	);
 
-	// Command area
-	Block::bordered()
-		.border_type(ratatui::widgets::BorderType::Rounded)
-		.fg(Color::LightGreen)
-		.title(" Zedditor ".to_span().into_centered_line())
-		.render(border_area, frame.buffer_mut());
+	let mut mode = app_state.mode.to_string();
+	mode = format!(" {mode} ");
+	frame.render_widget(
+		// Command area
+		Block::bordered()
+			.border_type(ratatui::widgets::BorderType::Rounded)
+			.fg(Color::LightGreen)
+			.title(mode),
+		layout[1],
+	);
 }
