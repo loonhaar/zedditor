71ccb015f2563cea2c82c3941de65a2eaf9b915b
cipher0ne <199516992+loonhaar@users.noreply.github.com>
2026-05-06 19:58:04 +0200

    Initial commit; Display a title and a border

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::{Ok, Result};
+use ratatui::{
+	DefaultTerminal, Frame,
+	crossterm::event::{self, Event},
+	layout::{Constraint, Layout},
+	style::{Color, Stylize},
+	text::ToSpan,
+	widgets::{Block, Widget},
+};
+
+fn main() -> Result<()> {
+	color_eyre::install()?;
+
+	let terminal = ratatui::init();
+	let result = run(terminal);
+
+	ratatui::restore();
+	result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+	loop {
+		terminal.draw(|t| render(t))?;
+
+		// TODO mode switching via key events
+		if let Event::Key(key) = event::read()? {
+			match key.code {
+				event::KeyCode::Esc => {
+					break;
+				}
+				_ => {}
+			}
+		}
+	}
+	Ok(())
+}
+
+fn render(frame: &mut Frame) {
+	let [border_area] = Layout::vertical([Constraint::Fill(1)])
+		.margin(0)
+		.areas(frame.area());
+
+	// Text area
+	Block::bordered()
+		.border_type(ratatui::widgets::BorderType::Rounded)
+		.fg(Color::LightGreen)
+		.title(" Zedditor ".to_span().into_centered_line())
+		.render(border_area, frame.buffer_mut());
+
+	// Command area
+	Block::bordered()
+		.border_type(ratatui::widgets::BorderType::Rounded)
+		.fg(Color::LightGreen)
+		.title(" Zedditor ".to_span().into_centered_line())
+		.render(border_area, frame.buffer_mut());
 }
